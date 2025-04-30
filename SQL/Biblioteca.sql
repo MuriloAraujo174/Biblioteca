5185c5c779e0259e1ad9ce95c4a1f183ad6dc0d5
Juliosantos10 <berlimbk@gmail.com>
2025-04-29 21:00:13 -0300

    adicionado sql com banco de dados e a pasta main

---
+++ SQL/Biblioteca.sql
@@ -0,0 +1,49 @@
+create database SistemaBiblioteca;
+use SistemaBiblioteca;
+create table Autores(
+    id_autores int primary key auto_increment,
+    nome varchar(50) not null,
+    nacionalidade varchar(50) not null,
+    editora varchar(50) not null,
+    codigo_autor int not null,
+    data_nascimento date not null
+);
+
+create Usuarios(
+    id_usuarios int primary key auto_increment,
+    nome_completo varchar(50) not null,
+    data_nascimento date,
+    cpf varchar(15) unique,
+    endereco varchar(100) not null,
+    telefone varchar(15) not null,
+    email varchar(50) not null,
+    senha varchar(20) not null
+);
+create funcionarios(
+    id_funcionarios int primary key auto_increment,
+    nome varchar(50) not null,
+    data_nascimento date not null,
+    endereco varchar(100) not null,
+    telefone varchar(15) not null,
+    email varchar(50) not null,
+    senha varchar(20) not null
+);
+create Livros(
+    id_livros int primary key auto_increment,
+    titulo varchar(100) not null,
+    genero varchar(50) not null,
+    ano_publicacao int not null,
+    codigo_livro int not null,
+    id_autores int not null,
+    foreign key (id_autores) references Autores(id_autores)
+);
+create Emprestimos(
+    id_emprestimos int primary key auto_increment,
+    id_livros int not null,
+    id_usuarios int not null,
+    data_emprestimo date not null,
+    data_devolucao date not null,
+    status varchar(20) not null,
+    foreign key (id_livros) references Livros(id_livros),
+    foreign key (id_usuarios) references Usuarios(id_usuarios)
+);
